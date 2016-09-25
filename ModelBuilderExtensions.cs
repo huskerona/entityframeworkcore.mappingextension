01c32e6384e9b35654512ea0465ee36915609007
huskerona <huskerona@outlook.com>
2016-09-25 03:34:29 +0200

    Initial upload

---
+++ ModelBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.MappingExtension
+{
+    public static class ModelBuilderExtensions
+    {
+        /// <summary>
+        /// Extension method for ModelBuilder that adds mapping for a model.
+        /// </summary>
+        /// <typeparam name="TEntity">Class to register with the Entity method.</typeparam>
+        /// <param name="modelBuilder">This instance of the ModelBuilder.</param>
+        /// <param name="mapping">Instance of an object that inherits from ModelTypeConfiguration class.</param>
+        /// <returns>ModelBuilder object.</returns>
+        public static ModelBuilder AddMapping<TEntity>(this ModelBuilder modelBuilder, ModelTypeConfiguration<TEntity> mapping)
+            where TEntity: class
+        {
+            return modelBuilder.Entity<TEntity>(mapping.Map);
+        }
+    }
+}
---
+++ ModelTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntityFrameworkCore.MappingExtension
+{
+    /// <summary>
+    /// Base class from which other class will inherit. Similar to EntityTypeConfiguration in EF6.x
+    /// </summary>
+    /// <typeparam name="TEntity">Class that will be configured.</typeparam>
+    public abstract class ModelTypeConfiguration<TEntity> where TEntity: class
+    {
+        /// <summary>
+        /// Method that is invoked by the <see cref="ModelBuilderExtensions.AddMapping{TEntity}"/> to configure the entity.
+        /// </summary>
+        /// <param name="builder">An instance of the API for configuring the properties.</param>
+        public abstract void Map(EntityTypeBuilder<TEntity> builder);
+    }
+}
